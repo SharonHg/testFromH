b332470d1974f80328758a5e23e0bc9843122d0f
TheresaHHH <44091390+TheresaHHH@users.noreply.github.com>
2023-09-28 19:03:52 -0400

    Create yourname.cpp

---
+++ yourname.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+class yourname.cpp {
+public:
+    void callname() {
+        std::cout << "what is your name";
+    }
+};
+
+int main() {
+    yourname obj;
+    obj.callname();
+    return 0;
+}
